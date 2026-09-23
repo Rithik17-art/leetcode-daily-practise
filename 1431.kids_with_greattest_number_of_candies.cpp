510fb0a6b68fb5cafd0b4c9959e07bb3f1a9854e
Rithik A G <rithikag.cs26@rvce.edu.in>
2026-09-23 22:02:15 +0530

    23.09.26(3)

---
+++ 1431.kids_with_greattest_number_of_candies.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int n=candies.size();
+        int max=*max_element(begin(candies),end(candies));
+        vector<bool> result(n);
+        for (int i=0;i<n;i++){
+            if (candies[i]+extraCandies>=max){
+                result[i]=true;
+            }
+            else{
+                result[i]=false;
+            }
+        }
+        return result;
+
+        }       
+    
+};
